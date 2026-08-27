01c521550f3560d1fdbe99e66896334730904d30
unknown <medinamati1107@gmail.com>
2026-08-26 23:10:40 -0300

    contenido

--- apps/views/publico/iniciox.php
+++ apps/views/publico/iniciox.php
@@ -4,7 +4,7 @@
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<title>Inicio</title>
-		<link rel="stylesheet" href="inicio.css" />
+		<link rel="stylesheet" href="iniciox.css" />
 	</head>
 	<body>
 		<header class="header">
